53237db89f8f330cb43a2766edb54d2d8a5ace31
The_heri24 <jose.heriberto@hotmail.es>
2025-02-04 22:05:35 -0500

    Primer commit

---
+++ GccApplication1/main.c
@@ -0,0 +1,17 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 4/02/2025 9:39:52 p. m.
+ * Author : marquez_final
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
